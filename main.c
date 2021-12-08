10883136971ca624167e79ae2bb99e1f88e87ade
João Pedro <joaopedrorafael19@gmail.com>
2021-12-08 17:45:44 -0300

    cabeçalho do arquivo

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
+/*@ 
+@autors: João Pedro Rafael, Alan Gabriel, Miguel Silva
+@motivation: Trabalho prático 1 de Algoritmos e estruturas de dados
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include "./utils/utils.c"
+
 
 int main(){
-  printf("Hello World! \n");
-  return 0;
+  
+  FILE *file;
+  char *filename, op;
+  do{
+    cls();
+    // menu de opções
+    printf("Opcoes:\n");
+    printf("1 - Ler arquivo de testes\n");
+    menu();
+    //menu de opções
+    op = getch();
+    
+    switch(op){
+      case '0':
+        exit(0);
+      case '1':
+        printf("Digite o nome do arquivo: ");
+        scanf("%s", filename);
+        fflush(stdin);
+        if(filename == NULL){
+          printf("Erro ao ler o arquivo\n");
+          break;
+        }
+        break;
+      default:
+        printf("Opcao invalida\n");
+    }
+  }while(op != 'q');
+
 }
