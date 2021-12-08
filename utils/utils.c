c67b8cda85765730b1273cacce10132a8aee88a8
João Pedro <joaopedrorafael19@gmail.com>
2021-12-08 18:31:57 -0300

    função para gerar identificador de processo

--- utils/utils.c
+++ utils/utils.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <time.h>
 
 #define cls() printf("\e[1;1H\e[2J");
 
@@ -6,4 +8,42 @@ void menu() {
   puts("0 - Sair");
   puts("1 - Ler arquivo de testes\n");
   puts("menu");
+}
+
+
+char * uid(int length)
+{
+  static int mySeed = 25011984;
+  char *string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
+  size_t stringLen = strlen(string);
+  char *randomString = NULL;
+
+  srand(time(NULL) * length + ++mySeed);
+
+  if (length < 1)
+  {
+    length = 1;
+  }
+
+  randomString = malloc(sizeof(char) * (length + 1));
+
+  if (randomString != NULL)
+  {
+    short key = 0;
+
+    for (int n = 0; n < length; n++)
+    {
+      key = rand() % stringLen;
+      randomString[n] = string[key];
+    }
+
+    randomString[length] = '\0';
+
+    return randomString;
+  }
+  else
+  {
+    printf("No memory");
+    exit(1);
+  }
 }
