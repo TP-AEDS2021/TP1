77cc8741f1ead552bc1a90ac317f6aee18e0fb12
AlanGabriews <ms.alangabriel@gmail.com>
2021-12-09 19:23:48 -0300

    tad processo

---
+++ processo.h
@@ -0,0 +1,33 @@
+#ifndef PROCESSO_H
+#define PROCESSO_H
+
+#include <time.h>
+
+//definicao do tipo
+typedef struct Tprocesso
+{
+    int PID;
+    int prioridade;
+    struct tm* horario_criacao;
+} Processo;
+
+//funcao para inicializar o processo
+void inicializa_processo(Processo*);
+
+//funcao para imprimir o processo
+void imprime_processo(Processo*);
+
+//gets e sets
+int get_PID(Processo*);
+
+void set_PID(Processo*, int);
+
+int get_prioridade(Processo*);
+
+void set_prioridade(Processo*, int);
+
+struct tm* get_horario_criacao(Processo*);
+
+void set_horario_criacao(Processo*, struct tm*);
+
+#endif
