b339eefaa82a3aaf9250c0b885ba37d0b03fe262
João Pedro <joaopedrorafael19@gmail.com>
2021-12-09 21:33:36 -0300

    implementação tipo processo, lista de proessos e funções

--- tads/processo/processo.c
+++ tads/processo/processo.c
@@ -20,32 +20,35 @@ void imprime_processo(Processo processo)
   return;
 }
 
-// gets e sets
+// retorna o PID do processo
 int get_PID(Processo *processo)
 {
   return processo->PID;
 }
 
+// define o PID do processo
 void set_PID(Processo *processo, int PID)
 {
   processo->PID = PID;
 }
 
+// retorna a prioridade do processo
 int get_prioridade(Processo *processo)
 {
   return processo->prioridade;
 }
 
+// define a prioridade do processo
 void set_prioridade(Processo *processo, int prioridade)
 {
   processo->prioridade = prioridade;
 }
-
+// retorna o horario de criacao do processo
 struct tm *get_horario_criacao(Processo *processo)
 {
   return processo->horario_criacao;
 }
-
+// define o horario de criacao do processo
 void set_horario_criacao(Processo *processo, struct tm *horario)
 {
   processo->horario_criacao = horario;
