a4a5616b5dae5e2350232abf98d36dc9833d8d14
João Pedro <joaopedrorafael19@gmail.com>
2021-12-17 00:00:53 -0300

    removendo funções de remocao e adição por indice

--- tads/lista_processos/lista_processo.h
+++ tads/lista_processos/lista_processo.h
@@ -26,8 +26,6 @@ Celula *inicializa_celula(Celula *, Processo *);
 void inicializa_celula_nula(Celula *, int);
 Lista * inicializa_lista(Lista *lista, int tamanho);
 void adiciona_celula(Lista *lista, Celula *celula);
-void adiciona_celula2(Lista *lista, Celula *celula);
-void remove_celula(Lista *lista, int indice);
 void remove_primeiro(Lista *lista);
 void imprime_lista(Lista *lista);
 
